d36b55e2ad6b72c78cf081248ebb73f30a1f3916
Katherine Fleming <katherinemarief@gmail.com>
2020-06-30 14:36:28 -0500

    final commit to submit

--- JS/app.js
+++ JS/app.js
@@ -1,26 +1,41 @@
 
-$(document).ready(() => {
-    var viewButton = $(":button").text("View Projects")
-    viewButton.show();
-
-    var changeButtonText = (() => {
-        $(":button").click(() => {
-            $(":button").text("View my projects");
-        })
-    });
-    var changeBack = (() => {
-        $(":button").click(() => {
-            $(":button").text("View my projects");
-        })
-    });
-
-
-    $(":button").click(() => {
-        var hideButton = $(":button").text("Hide Projects")
-        $('.card').toggle();
-        hideButton.show();
-        changeButtonText();
+$(document).ready(()=>{
+    $("#filter-item-web").click(()=>{
+      $(".card").filter(document.getElementsByClassName("web"));
     })
+    $("#filter-item-brand").click(()=>{
+      $(".card").filter(document.getElementsByClassName("brand"));
+    })
+    $("#filter-item-print").click(()=>{
+      $(".card").filter(document.getElementsByClassName("print"));
+    })
+}) 
+
+
+// $(document).ready(() => {
+//     var viewButton = $(":button").text("View Services")
+//     viewButton.show();
+
+    
+
+    // var changeButtonText = (() => {
+    //     $(":button").click(() => {
+    //         $(":button").text("View projects");
+    //     })
+    // });
+    // var changeBack = (() => {
+    //     $(":button").click(() => {
+    //         $(":button").text("View my projects");
+    //     })
+    // });
+
+
+    // $(":button").click(() => {
+    //     var hideButton = $(":button").text("Hide Projects")
+    //     $('.card').toggle();
+    //     hideButton.show();
+    //     changeButtonText();
+    // })
 
 
 
@@ -28,5 +43,4 @@ $(document).ready(() => {
     //     $("#nav-placeholder").load("navbar.html");
     //   });
 
-
-});
+// });
